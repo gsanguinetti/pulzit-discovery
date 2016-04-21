39c960c1475ac62179bb0f6cd01f7a1b07698052
Gaston Sanguinetti <gaston.sanguinetti@despegar.com>
2016-04-20 23:39:18 -0300

    proyect created

---
+++ app/src/test/java/com/pulzit/discovery/DiscoveryUnitTest.java
@@ -0,0 +1,12 @@
+package com.pulzit.discovery;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class DiscoveryUnitTest {
+    //todo: create unit tests
+}
